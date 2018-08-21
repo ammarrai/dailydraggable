709e9ece34fe5f0cae55bb097c6251c0b6316990
Unknown <ammarwdc@gmail.com>
2018-08-21 10:28:10 -0400

    fixed contdown.js

--- countdown.js
+++ countdown.js
@@ -1,90 +1,41 @@
 $(document).ready(function () {
 
-    var bt = localStorage.getItem('bedTime');
-    var wt = localStorage.getItem('wakeTime');
-
-    //  console.log('Bed Time:' + bt);
-    //  console.log('Daily Available time' + dat);
-    //  console.log('Wake up time:' + wt);
-
-    var dateNow = moment().format('MMM D, YYYY');
-    var placeHolderDate = dateNow + " " + bt;
-
-    var countDownDate = new Date(placeHolderDate).getTime();
-
-    var x = setInterval(function () {
-
-        // Get todays date and time
-        var now = new Date().getTime();
-
-        // Find the distance between now and the count down date
-        var distance = countDownDate - now;
-
-        // Time calculations for days, hours, minutes and seconds
-        var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-        $("#countDown").val(hours + ":" + minutes + ":" + seconds);
-
-        // If the countdown is over, write some text
-        if (hours === 0 && minutes === 0 && seconds === 0) {
-            $("#countDown").val("00:00:00");
-            document.getElementById('countDown').style.color = 'red';
-        }
-
-        if (hours < 0 || minutes < 0 || seconds < 0) {
-            $("#countDown").val("00:00:00");
-            document.getElementById('countDown').style.color = 'red';
-        }
-
-        var timeNow = moment().format('HH:mm');
-        if (timeNow === wt) {
-            clearInterval(x);
-            restartCountdown();
-        }
-
-    }, 1000);
-
-
-    function restartCountdown() {
-
+    function countdown() {
         var bt = localStorage.getItem('bedTime');
+        var wt = localStorage.getItem('wakeTime');
 
-        var dN = (moment().add(moment.duration({d: 1})).format('MMM D, YYYY'));
 
-        var placeHolderDate = dN + " " + bt;
-        var countDownDate = new Date(placeHolderDate).getTime();
+        var today = new Date(),
+            dd = today.getDate(),
+            mm = today.getMonth() + 1,
+            yyyy = today.getFullYear();
 
-        // Update the count down every 1 second
-        var x = setInterval(function () {
+        var startTime = new Date(mm + '/' + dd + '/' + yyyy + ' ' + wt),
+            endTime = new Date(mm + '/' + dd + '/' + yyyy + ' ' + bt);
 
-            // Get todays date and time
-            var now = new Date().getTime();
-
-            // Find the distance between now and the count down date
-            var distance = countDownDate - now;
-
-            // Time calculations for days, hours, minutes and seconds
-            var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-            var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-            var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-            $("#countDown").val(hours + ":" + minutes + ":" + seconds);
-
-            // If the countdown is over, write some text
-            if (hours === 0 && minutes === 0 && seconds === 0) {
-                $("#countDown").val("00:00:00");
-                document.getElementById('countDown').style.color = '#f08000';
-                alert('this');
+        setInterval(function () {
+            var now = new Date();
+            var nowdd = today.getDate();
+            var nowTime = now.getTime();
+            if (dd !== nowdd) {
+                dd = nowdd;
+                startTime = new Date(dd + '/' + mm + '/' + yyyy + ' wt');
+                endTime = new Date(dd + '/' + mm + '/' + yyyy + ' bt');
             }
 
-            if (hours < 0 || minutes < 0 || seconds < 0) {
-                $("#countDown").val("00:00:00");
-                document.getElementById('countDown').style.color = '#f08000';
-                alert('this');
-            }
+            if (nowTime > startTime && nowTime < endTime) {
 
+                var distance = endTime - nowTime;
+
+                var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)),
+                    minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60)),
+                    seconds = Math.floor((distance % (1000 * 60)) / 1000);
+                $("#countDown").val(hours + ":" + minutes + ":" + seconds);
+            } else {
+                $("#countDown").val("00:00:00");
+            }
         }, 1000);
     }
+
+    countdown();
 });
